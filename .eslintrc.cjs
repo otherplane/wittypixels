dcc25506a69d44bc2ce4887b8be84b9f7d7cad10
gabaldon <maria@otherplane.com>
2023-01-11 13:57:52 +0100

    feat: implement canvas

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,6 @@ module.exports = {
   ],
   rules: {
     'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   }
 }
