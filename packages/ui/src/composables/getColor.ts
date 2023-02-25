4709635cd356f09acc6901b13fc51952e609e63c
tommy <tomastrg22@gmail.com>
2023-02-25 13:21:09 -0700

    Revert "logger"
    This reverts commit 1eadbf045b1815866d8c28c6999d7926056926f8.

--- packages/ui/src/composables/getColor.ts
+++ packages/ui/src/composables/getColor.ts
@@ -7,20 +7,15 @@ export function getColor(
   color: number | null = null,
   shade: number | null = null
 ) {
-  console.log(1)
   const store = useStore()
-  console.log(2)
   const selectedShade = computed(() =>
     isNumber(shade) ? shade : store.selectedShade
   )
-  console.log(3)
   const selectedColor = isNumber(color)
     ? COLORS[color]
     : COLORS[store.selectedColor]
-  console.log(4)
   const shadeData = selectedColor
     ? selectedColor[selectedShade.value]
     : [255, 255, 255]
-  console.log(5)
   return computed(() => getRgbaColor(shadeData[0], shadeData[1], shadeData[2]))
 }
